d4709dcf3e422a95850a5c11f29c9884b3333e1e
Shaun Kahler <shaun.kahler@gmail.com>
2026-05-29 19:56:19 -0400

    build: Add Gradle build scripts for Android Studio

---
+++ ProjectOmnis/app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.projectomnis.shell"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.projectomnis.shell"
+        minSdk = 29
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++20"
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    externalNativeBuild {
+        cmake {
+            path("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    // No Java dependencies needed, pure C++!
+}
